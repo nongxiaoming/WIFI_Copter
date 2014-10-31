142a0e60adf668d3d0e8470e45af62d6d453dc1b
nongxiaoming <nongxiaoming@gmail.com>
2014-10-31 14:49:37 +0800

    update mpu6050 driver.

--- software/firmware/Drivers/mpu6050.c
+++ software/firmware/Drivers/mpu6050.c
@@ -1,30 +1,29 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <finsh.h>
-#include "mpu6500.h"
+#include "mpu6050.h"
 
-#define CONFIG_REG_NUM 7
-rt_uint8_t MPU_Init_Data[CONFIG_REG_NUM][2] =
-{
-    {MPU6500_PWR_MGMT_1, 0x80},    // Reset Device
-    {MPU6500_PWR_MGMT_1, 0x01},    // Clock Source
-    {MPU6500_PWR_MGMT_2, 0x00},    // Enable Acc & Gyro
-    {MPU6500_GYRO_CONFIG, 0x18},   // +-2000dps
-    {MPU6500_ACCEL_CONFIG, 0x08},  // +-4G
-    {MPU6500_ACCEL_CONFIG_2, 0x09}, // Set Acc Data Rates, Enable Acc LPF , Bandwidth 184Hz
-    {MPU6500_INT_PIN_CFG, 0x30},   //
-};
 
-static rt_size_t mpu6500_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size)
+static void mpu6050_write_bit(rt_device_t dev,rt_uint8_t reg, rt_uint8_t bitNum, rt_uint8_t data);
+static void mpu6050_write_bits(rt_device_t dev,rt_uint8_t reg, rt_uint8_t bitStart, rt_uint8_t length,rt_uint8_t data);
+static void mpu6050_setClockSource(rt_device_t dev,rt_uint8_t source);
+static void mpu6050_setFullScaleGyroRange(rt_device_t dev,rt_uint8_t range);
+static void mpu6050_setFullScaleAccelRange(rt_device_t dev,rt_uint8_t range);
+static void mpu6050_setSleepEnabled(rt_device_t dev,rt_uint8_t enabled);
+static void mpu6050_setI2CMasterModeEnabled(rt_device_t dev,rt_uint8_t enabled);
+static void mpu6050_setI2CBypassEnabled(rt_device_t dev,rt_uint8_t enabled);
+static void mpu6050_setDLPF(rt_device_t dev,rt_uint8_t mode);
+
+static rt_size_t mpu6050_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size)
 {
     struct rt_i2c_msg msgs[2];
-    mpu6500_dev_t *mpu6500;
+    mpu6050_dev_t *mpu6500;
     rt_uint8_t read_reg = pos;
 
     if (dev->user_data == RT_NULL)
         return (0);
 
-    mpu6500 = (mpu6500_dev_t *)dev->user_data;
+    mpu6500 = (mpu6050_dev_t *)dev->user_data;
 
     msgs[0].addr  = mpu6500->addr;
     msgs[0].flags = RT_I2C_WR;
@@ -42,15 +41,15 @@ static rt_size_t mpu6500_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_si
         return (0);
 }
 
-static rt_size_t mpu6500_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size)
+static rt_size_t mpu6050_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size)
 {
     struct rt_i2c_msg msgs[2];
-    mpu6500_dev_t *mpu6500;
+    mpu6050_dev_t *mpu6500;
     rt_uint8_t write_reg = pos;
 
     if (dev->user_data == RT_NULL)
         return (0);
-    mpu6500 = (mpu6500_dev_t *)dev->user_data;
+    mpu6500 = (mpu6050_dev_t *)dev->user_data;
 
     msgs[0].addr  = mpu6500->addr;
     msgs[0].flags = RT_I2C_WR;
@@ -72,17 +71,26 @@ static rt_size_t mpu6500_write(rt_device_t dev, rt_off_t pos, const void *buffer
 /**
  * The following is rt-thread device operating interface
  */
-static rt_err_t mpu6500_init(rt_device_t dev)
+static rt_err_t mpu6050_init(rt_device_t dev)
 {
-    rt_uint8_t i = 0;
-    for (i = 0; i < CONFIG_REG_NUM; i++)
-    {
-        mpu6500_write(dev, MPU_Init_Data[i][0], &MPU_Init_Data[i][1], 1);
-    }
+  mpu6050_setSleepEnabled(dev,0); //进入工作状态
+  rt_thread_delay(1);
+	mpu6050_setClockSource(dev,MPU6050_CLOCK_PLL_XGYRO); //设置时钟  0x6b   0x01
+	rt_thread_delay(1);
+	mpu6050_setFullScaleGyroRange(dev,MPU6050_GYRO_FS_2000);//陀螺仪最大量程 +-2000度每秒
+	rt_thread_delay(1);
+	mpu6050_setFullScaleAccelRange(dev,MPU6050_ACCEL_FS_4);	//加速度度最大量程 +-4G
+	rt_thread_delay(1);
+	mpu6050_setDLPF(dev,MPU6050_DLPF_BW_42);
+	rt_thread_delay(1);
+	mpu6050_setI2CMasterModeEnabled(dev,0);	 //不让MPU6050 控制AUXI2C
+	rt_thread_delay(1);
+	mpu6050_setI2CBypassEnabled(dev,1);	
+	
     return RT_EOK;
 }
 
-static rt_err_t mpu6500_open(rt_device_t dev, rt_uint16_t oflag)
+static rt_err_t mpu6050_open(rt_device_t dev, rt_uint16_t oflag)
 {
 
     if (dev->user_data == RT_NULL)
@@ -95,24 +103,24 @@ static rt_err_t mpu6500_open(rt_device_t dev, rt_uint16_t oflag)
     }
 }
 
-static rt_err_t mpu6500_close(rt_device_t dev)
+static rt_err_t mpu6050_close(rt_device_t dev)
 {
     return RT_EOK;
 }
-static rt_err_t mpu6500_control(rt_device_t dev, rt_uint8_t cmd, void *args)
+static rt_err_t mpu6050_control(rt_device_t dev, rt_uint8_t cmd, void *args)
 {
     return RT_EOK;
 }
-void rt_hw_mpu6500_init(const char *i2c_bus_name, rt_uint16_t addr)
+void rt_hw_mpu6050_init(const char *i2c_bus_name, rt_uint16_t addr)
 {
     rt_device_t device;
-    mpu6500_dev_t *mpu6500;
+    mpu6050_dev_t *mpu6050;
     RT_ASSERT(i2c_bus_name != RT_NULL);
-    mpu6500 = (mpu6500_dev_t *)rt_malloc(sizeof(mpu6500_dev_t));
+    mpu6050 = (mpu6050_dev_t *)rt_malloc(sizeof(mpu6050_dev_t));
 
-    mpu6500->i2c_bus = (struct rt_i2c_bus_device *)rt_device_find(i2c_bus_name);
-    RT_ASSERT(mpu6500->i2c_bus != RT_NULL);
-    mpu6500->addr = addr;
+    mpu6050->i2c_bus = (struct rt_i2c_bus_device *)rt_device_find(i2c_bus_name);
+    RT_ASSERT(mpu6050->i2c_bus != RT_NULL);
+    mpu6050->addr = addr;
 
 
     device = (rt_device_t)rt_malloc(sizeof(struct rt_device));
@@ -121,39 +129,91 @@ void rt_hw_mpu6500_init(const char *i2c_bus_name, rt_uint16_t addr)
     device->type        = RT_Device_Class_Char;
     device->rx_indicate = RT_NULL;
     device->tx_complete = RT_NULL;
-    device->init        = mpu6500_init;
-    device->open        = mpu6500_open;
-    device->close       = mpu6500_close;
-    device->read        = mpu6500_read;
-    device->write       = mpu6500_write;
-    device->control     = mpu6500_control;
-    device->user_data   = mpu6500;
+    device->init        = mpu6050_init;
+    device->open        = mpu6050_open;
+    device->close       = mpu6050_close;
+    device->read        = mpu6050_read;
+    device->write       = mpu6050_write;
+    device->control     = mpu6050_control;
+    device->user_data   = mpu6050;
 
-    rt_device_register(device, "mpu6500", RT_DEVICE_FLAG_RDWR);
+    rt_device_register(device, "mpu6050", RT_DEVICE_FLAG_RDWR);
+}
+static void mpu6050_write_bit(rt_device_t dev,rt_uint8_t reg, rt_uint8_t bitNum, rt_uint8_t data)
+{
+	rt_uint8_t value;
+	mpu6050_read(dev,reg,&value,1);
+	value = (data != 0) ? (value | (1 << bitNum)) : (value & ~(1 << bitNum));
+	mpu6050_write(dev,reg,&value,1);
+}
+static void mpu6050_write_bits(rt_device_t dev,rt_uint8_t reg, rt_uint8_t bitStart, rt_uint8_t length,rt_uint8_t data)
+{
+	rt_uint8_t value,mask;
+	mpu6050_read(dev,reg,&value,1);
+	mask = (0xFF << (bitStart + 1)) | 0xFF >> ((8 - bitStart) + length - 1);
+	data <<= (8 - length);
+	data >>= (7 - bitStart);
+	value &= mask;
+	value |= data;
+	mpu6050_write(dev,reg,&value,1);
+}
+static void mpu6050_setClockSource(rt_device_t dev,rt_uint8_t source)
+{
+	mpu6050_write_bits(dev,MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_CLKSEL_BIT, MPU6050_PWR1_CLKSEL_LENGTH, source);	
 }
 
-rt_uint8_t mpu6500_test(void)
+static void mpu6050_setFullScaleGyroRange(rt_device_t dev,rt_uint8_t range) 
 {
-    rt_device_t mpu6500 = RT_NULL;
-    rt_uint8_t mpu6500_id = 0;
+	mpu6050_write_bits(dev,MPU6050_RA_GYRO_CONFIG, MPU6050_GCONFIG_FS_SEL_BIT, MPU6050_GCONFIG_FS_SEL_LENGTH, range);
+}
+
+static void mpu6050_setFullScaleAccelRange(rt_device_t dev,rt_uint8_t range) 
+{
+	mpu6050_write_bits(dev,MPU6050_RA_ACCEL_CONFIG, MPU6050_ACONFIG_AFS_SEL_BIT, MPU6050_ACONFIG_AFS_SEL_LENGTH, range);
+}
+
+static void mpu6050_setSleepEnabled(rt_device_t dev,rt_uint8_t enabled) 
+{
+	mpu6050_write_bit(dev,MPU6050_RA_PWR_MGMT_1, MPU6050_PWR1_SLEEP_BIT, enabled);
+}
+
+static void mpu6050_setI2CMasterModeEnabled(rt_device_t dev,rt_uint8_t enabled) 
+{
+	mpu6050_write_bit(dev,MPU6050_RA_USER_CTRL, MPU6050_USERCTRL_I2C_MST_EN_BIT, enabled);
+}
+
+static void mpu6050_setI2CBypassEnabled(rt_device_t dev,rt_uint8_t enabled) 
+{
+	mpu6050_write_bit(dev,MPU6050_RA_INT_PIN_CFG, MPU6050_INTCFG_I2C_BYPASS_EN_BIT, enabled);
+}
+
+static void mpu6050_setDLPF(rt_device_t dev,rt_uint8_t mode)
+{
+	mpu6050_write_bits(dev,MPU6050_RA_CONFIG, MPU6050_CFG_DLPF_CFG_BIT, MPU6050_CFG_DLPF_CFG_LENGTH, mode);
+}
+
+rt_uint8_t mpu6050_test(void)
+{
+    rt_device_t mpu6050 = RT_NULL;
+    rt_uint8_t mpu6050_id = 0;
     rt_uint8_t mpu_data[14];
     rt_uint16_t acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z;
-    mpu6500 = rt_device_find("mpu6500");
-    while (mpu6500 == RT_NULL)
+    mpu6050 = rt_device_find("mpu6050");
+    while (mpu6050 == RT_NULL)
     {
-        rt_kprintf("can not find mpu6500!\n");
-        rt_hw_mpu6500_init("i2c1", MPU6500_DEFAULT_ADDRESS);
+        rt_kprintf("can not find mpu6050!\n");
+        rt_hw_mpu6050_init("i2c1", MPU6050_DEFAULT_ADDRESS);
         rt_thread_delay(5);
-        mpu6500 = rt_device_find("mpu6500");
+        mpu6050 = rt_device_find("mpu6050");
     }
-    rt_device_open(mpu6500, RT_DEVICE_FLAG_RDONLY);
-    mpu6500->read(mpu6500, MPU6500_WHOAMI, &mpu6500_id, 1);
-    rt_kprintf("mpu6500 id = %x\n", mpu6500_id);
-    if (mpu6500_id != 0x70)
+    rt_device_open(mpu6050, RT_DEVICE_FLAG_RDONLY);
+    mpu6050->read(mpu6050, MPU6050_RA_WHO_AM_I, &mpu6050_id, 1);
+    rt_kprintf("mpu6050 id = %x\n", mpu6050_id);
+    if (mpu6050_id != 0x68)
         return 0;
     while (1)
     {
-        mpu6500->read(mpu6500, MPU6500_ACCEL_XOUT_H, mpu_data, 14);
+        mpu6050->read(mpu6050, MPU6050_RA_ACCEL_XOUT_H, mpu_data, 14);
         acc_x = ((mpu_data[0] << 8) | mpu_data[1]);
         acc_y = ((mpu_data[2] << 8) | mpu_data[3]);
         acc_z = ((mpu_data[4] << 8) | mpu_data[5]);
@@ -166,4 +226,4 @@ rt_uint8_t mpu6500_test(void)
     }
     //return 0 ;
 }
-FINSH_FUNCTION_EXPORT(mpu6500_test, mpu6500 test);
+FINSH_FUNCTION_EXPORT(mpu6050_test, mpu6500 test);
