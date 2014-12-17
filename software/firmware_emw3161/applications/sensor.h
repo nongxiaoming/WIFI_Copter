ee7bf3d450afa82f2de2922f9f1c3999a45784dd
nongxiaoming <nongxiaoming@gmail.com>
2014-12-17 10:29:42 +0800

    加入sensor相关处理

---
+++ software/firmware_emw3161/applications/sensor.h
@@ -0,0 +1,35 @@
+#ifndef __SENSOR_H
+#define __SENSOR_H
+
+#include <rtthread.h>
+#include "params.h"
+#include "ANO_Math.h"
+
+#define CALIBRATING_GYRO_CYCLES             1000
+#define CALIBRATING_ACC_CYCLES              400
+
+#define MPU6050G_S250DPS            ((float)0.0076335f)  // 0.0087500 dps/LSB
+#define MPU6050G_S500DPS            ((float)0.0152671f)  // 0.0175000 dps/LSB
+#define MPU6050G_S2000DPS           ((float)0.0609756f)  // 0.0700000 dps/LSB
+	
+struct Sensor
+{
+	rt_bool_t Acc_CALIBRATED;
+	rt_bool_t Gyro_CALIBRATED;
+	vector3i_t Acc_Offset,Gyro_Offset;
+};
+
+	//初始化6050
+	void Sensor_Init(uint16_t sample_rate, uint16_t lpf);
+	//读取加速度 角速度
+	void Sensor_ReadData(void);
+	//返回加速度的值
+	vector3f_t Sensor_GetAcc(void);
+	//返回角速度的值
+	vector3f_t Sensor_GetGyro(void);
+	//返回单位为度每秒的角速度
+	vector3f_t Sensor_GetGyro_in_dps(void);
+extern struct Sensor sensor;
+
+
+#endif
