b0885c2bf7c68cc0a7881bab86d1ad7608bc1842
xiaonong <nongxiaoming@gmail.com>
2015-01-03 23:20:51 +0800

    update the telnet.

--- software/firmware/applications/telnet.h
+++ software/firmware/applications/telnet.h
@@ -1,13 +1,6 @@
 #ifndef __TELNET_H
 #define __TELNET_H
-#ifdef __cplusplus
- extern "C" {
-#endif /* __cplusplus */
-	 
-void telnet_server_init(void);
 
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
+void telnet_server_init(void);
 
 #endif
